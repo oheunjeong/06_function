de7daa51601e5fa8771ff98155807d906a377dfa
oheunjeong <55427561+oheunjeong@users.noreply.github.com>
2019-10-10 15:31:24 +0900

    06

--- main.c
+++ main.c
@@ -5,15 +5,18 @@
 
 int main(int argc, char *argv[]) {
 	
+	void print_star()
 	{
 		int i;
 		for(i=0;i<10;i++)
-			printf("*");
-		for(i=0;i<10;i++)
-			printf("*");
-		for(i=0;i<10;i++)
-			printf("*");
+		printf("*");
 	}
-	
+	int main(void)
+	{
+		print_star();
+		print_star();
+		print_star();
+		
+	}
 	return 0;
 }
