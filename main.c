3e214b468b9ce17913f3688e3e1bc48ac65f7d42
oheunjeong <55427561+oheunjeong@users.noreply.github.com>
2019-10-15 16:39:42 +0900

    06

--- main.c
+++ main.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-#include <stdlib.h>
+#include <stdio.h>
 #include <time.h>
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 int factorial(int n)
